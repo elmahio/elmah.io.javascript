832f907041d315ac4eef2a0ca4a88537d2464061
Stanescu Eduard-Dan <zflashare@gmail.com>
2018-07-27 14:31:21 +0300

    Update elmahio-logger.js

--- elmahio-logger.js
+++ elmahio-logger.js
@@ -136,8 +136,15 @@
 
 	function merge_objects(obj1, obj2) {
 		var obj3 = {};
-		for (var attrname in obj1) { obj3[attrname] = obj1[attrname]; }
-		for (var attrname in obj2) { obj3[attrname] = obj2[attrname]; }
+		for (var attrname in obj1) {
+			obj3[attrname] = obj1[attrname];
+		}
+		for (var attrname in obj2) {
+			if (attrname !== 'title' && attrname !== 'severity') {
+				obj3[attrname] = obj2[attrname];
+			}
+		}
+
 		return obj3;
 	}
 
