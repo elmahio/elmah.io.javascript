51716496a559dac3a1843613f1c623b0ccf232d0
Stanescu Eduard-Dan <zflashare@gmail.com>
2018-08-27 20:36:04 +0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -4,34 +4,14 @@ var sourcemaps = require('gulp-sourcemaps');
 var strip = require('gulp-strip-comments');
 var beautify = require('gulp-beautify');
 
-gulp.task('source', function(){
-  return gulp.src('src/elmahio.js')
-    .pipe(strip({ safe: true }))
-    .pipe(beautify({ indent_size: 2, preserve_newlines: false }))
-    .pipe(gulp.dest('dist'))
-});
-
-gulp.task('source-map', function(){
+gulp.task('create-dist', function () {
   return gulp.src('src/elmahio.js')
     .pipe(sourcemaps.init())
     .pipe(strip({ safe: true }))
     .pipe(beautify({ indent_size: 2, preserve_newlines: false }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist'))
-});
-
-gulp.task('minify', function () {
-  return gulp.src('src/elmahio.js')
-    .pipe(minify({ ext: { min: '.min.js' }, preserveComments: 'some' }))
-    .pipe(gulp.dest('dist'))
-});
-
-gulp.task('minify-map', function () {
-  return gulp.src('src/elmahio.js')
-    .pipe(sourcemaps.init())
     .pipe(minify({ ext: { min: '.min.js' }, preserveComments: 'some' }))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist'))
 });
 
-gulp.task('default', ['source', 'source-map', 'minify', 'minify-map']);
+gulp.task('default', ['create-dist']);
