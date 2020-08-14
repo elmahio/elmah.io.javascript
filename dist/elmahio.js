15db2d9343f9aed8af7415599da6dc3b2a53eb8f
Stanescu Eduard-Dan <zflashare@gmail.com>
2020-08-14 12:48:54 +0300

    Changed version number

--- dist/elmahio.js
+++ dist/elmahio.js
@@ -1,5 +1,5 @@
 /*!
- * elmah.io Javascript Logger - version 3.1.4
+ * elmah.io Javascript Logger - version 3.3.0
  * (c) 2018 elmah.io, Apache 2.0 License, https://elmah.io
  */
 (function(root, factory) {
