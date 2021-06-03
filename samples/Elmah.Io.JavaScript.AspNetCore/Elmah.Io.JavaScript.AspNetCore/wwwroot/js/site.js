28b99c4f7e8ef7d253845e28e98fae3b74959f5b
Thomas Ardal <thomasardal@gmail.com>
2021-06-03 08:28:03 +0200

    Improved asp.net core sample

--- samples/Elmah.Io.JavaScript.AspNetCore/Elmah.Io.JavaScript.AspNetCore/wwwroot/js/site.js
+++ samples/Elmah.Io.JavaScript.AspNetCore/Elmah.Io.JavaScript.AspNetCore/wwwroot/js/site.js
@@ -2,11 +2,18 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
-new Elmahio({
+var logger = new Elmahio({
     apiKey: 'API_KEY',
-    logId: 'LOG_ID'
+    logId: 'LOG_ID',
+    debug: true,
+    breadcrumbs: true
 });
 
 function generateError() {
     throw new Error("This is a test error that goes into your log on elmah.io");
+}
+
+function addBreadcrumb() {
+    debugger;
+    logger.addBreadcrumb("Information", "Log", "Something happened before the error");
 }
